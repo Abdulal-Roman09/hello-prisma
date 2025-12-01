3e94f0c94a90b17b3e22ade0f326743ae0098522
Abdul Al Roman <mdroman45678910@gmail.com>
2025-12-01 18:44:03 +0600

    feat:create data in differnts model

--- src/create.ts
+++ src/create.ts
@@ -4,17 +4,53 @@ async function main() {
 
     // Create a new user with a post
 
-    const user = await prisma.user.create({
+    // const user = await prisma.user.create({
+    //     data: {
+    //         username: 'noman',
+    //         email: 'noman3@gamil.com',
+    //         role: "user"
+
+    //     },
+    // })
+
+    // const getallUser = await prisma.user.findMany()
+    // console.log('all user:', getallUser)
+
+
+    // create profile
+    // const createPofile = await prisma.profile.create({
+    //     data: {
+    //         bio: "this is bio",
+    //         userId: 1
+    //     }
+    // })
+    // const createCatagory = await prisma.category.create(
+    //     {
+
+    //         data: {
+    //             name: "nextjs"
+    //         }
+    //     }
+    // )
+    const createPost = await prisma.post.create({
         data: {
-            username: 'noman',
-            email: 'noman3@gamil.com',
-            role: "user"
-
+            title: "this is title",
+            content: "this is the post of the contenets",
+            authorId: 1,
+            postCategory: {
+                create: {
+                    category: {
+                        connect: { id: 1 }
+                    }
+                }
+            }
         },
+        include: {
+            postCategory: true
+        }
     })
+    console.log(createPost)
 
-    const getallUser = await prisma.user.findMany()
-    console.log('all user:', getallUser)
 
 }
 main()
