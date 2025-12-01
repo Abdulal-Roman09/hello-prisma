4e005fe05e168d3dd8d0433d3bda5752ed38ac21
Abdul Al Roman <mdroman45678910@gmail.com>
2025-12-01 23:56:34 +0600

    feat :filter differnet type of query

--- src/filtering.ts
+++ src/filtering.ts
@@ -5,38 +5,85 @@ const filtering = async () => {
 
     // and filtering
 
-    const andfiltering = await prisma.post.findMany({
-        where: {
-            AND: [
-                {
-                    title: {
-                        contains: "title"
-                    }
-                },
-                {
-                    published: false
-                }
-            ]
-        }
-    })
-    console.log(andfiltering)
+    // const andfiltering = await prisma.post.findMany({
+    //     where: {
+    //         AND: [
+    //             {
+    //                 title: {
+    //                     contains: "title"
+    //                 }
+    //             },
+    //             {
+    //                 published: false
+    //             }
+    //         ]
+    //     }
+    // })
+    // console.log(andfiltering)
 
     // or filtering
 
-    const orfiltering = await prisma.post.findMany({
+    // const orfiltering = await prisma.post.findMany({
+    //     where: {
+    //         OR: [
+    //             {
+    //                 title: {
+    //                     contains: "title"
+    //                 }
+    //             },
+    //             {
+    //                 published: false
+    //             }
+    //         ]
+    //     }
+    // })
+    // console.log(orfiltering)
+
+    // not filtering 
+    // const notFiltering = await prisma.post.findMany({
+    //     where: {
+    //         NOT: [
+    //             {
+    //                 title: {
+    //                     contains: "title"
+    //                 }
+    //             }
+    //         ]
+    //     }
+    // })
+    // console.log(notFiltering)
+
+
+    // start with
+    const startWithFiltering = await prisma.user.findMany({
         where: {
-            OR: [
-                {
-                    title: {
-                        contains: "title"
-                    }
-                },
-                {
-                    published: false
-                }
-            ]
+            email: {
+                startsWith: "com"
+            }
         }
+
     })
-    console.log(orfiltering)
+    console.log(startWithFiltering)
+
+    // end with
+    const endWithFiltering = await prisma.user.findMany({
+        where: {
+            email: {
+                endsWith: "title"
+            }
+        }
+
+    })
+    console.log(endWithFiltering)
+    // end with
+    const equalFiltering = await prisma.user.findMany({
+        where: {
+            email: {
+                equals: "title"
+            }
+        }
+
+    })
+    console.log(equalFiltering)
 }
 filtering()
